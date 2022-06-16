8e41f729dcb960d28a512dc1fe34054e3147cf94
Brandon Mogren <mogren@hotmail.se>
2022-06-16 23:02:57 +0200

    Added so that bot will send replies when pinging certain user Added missing asyncs and await

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { AudioPlayerStatus, createAudioPlayer, createAudioResource, DiscordGatewayAdapterCreator, joinVoiceChannel, VoiceConnection } from "@discordjs/voice";
 import { Client, Collection, CommandInteraction, Intents, VoiceState } from "discord.js";
 import { join } from "path";
-require("dotenv").config({path: ".env"});
 import * as fs from "fs";
 import { SlashCommandBuilder } from "@discordjs/builders";
+require("dotenv").config({path: ".env"});
 
 interface ClientExtended extends Client {
     commands?: Collection<string, {data: SlashCommandBuilder, execute: (interaction: CommandInteraction) => Promise<void>}>;
@@ -31,7 +31,7 @@ client.on("guildCreate", (guild) => {
     }
 });
 
-client.on("voiceStateUpdate", (oldState, newState) => {
+client.on("voiceStateUpdate", async (oldState, newState) => {
     if (newState.member.user.bot) return;
 
     if (!oldState.channelId && newState.channelId) {
@@ -42,18 +42,29 @@ client.on("voiceStateUpdate", (oldState, newState) => {
         }
 
         if (secretSongActive && newState.channel.members.size === 2 && newState.channel.members.some(u => u.id === "226326393783451648") && newState.channel.members.some(u => u.id === "443816218646937602")) {
-            PlaySong(newState, "sound/speechless.mp3");
+            await PlaySong(newState, "sound/speechless.mp3");
         } else if (newState.member.user.id === "443816218646937602" && newState.channel.members.size > 1) {
-            PlaySong(newState, Math.floor(Math.random()*100)+1 === 99 ? "sound/theme.mp3" : "sound/Cherry_bitch.mp3");
+            await PlaySong(newState, Math.floor(Math.random() * 100) + 1 === 99 ? "sound/theme.mp3" : "sound/Cherry_bitch.mp3");
         }
 
     }
  });
 
+client.on("messageCreate", async msg => {
+    if (msg.author.bot) return;
+
+    if (msg.content.startsWith("<@!443816218646937602>") || msg.content.startsWith("<@443816218646937602>")) {
+        let roasts = ["You're as useless as the \"ueue\" in \"queue\"", "Mirrors can't talk. Lucky for you they can't laugh either", "hey, you have something on your chin... no, the 3rd one down", "You're the reason the gene pool needs a lifeguard.", "If i had a face like yours, I'd sue my parents.", "Your only chance of getting laid is to crawl up a chicken's butt and wait.", "Some day you'll go far... andi hope you stay there.", "Aha! I see the fuck-up fairy has visited us again", "You must have been born on a highway cus' that's where most accidents happen", "If laughter is the best medicine, your face must be curing the world", "Is your ass jealous of the amount of shit that just came out of your mouth?", "So a thought crossed your mind? Must have been a long and lonely journey", "If i wanted to kill myself I'd climb your ego and jump to your IQ", "I'd agree with you but then we'd both be wrong", "When I see your face there's not a thing I would change... except the direction I was looking in", "If i had a dollar for every time you said something smart, I'd be broke.", "When you were born the doctor threw you out the window, but the window threw you back.", "You're the reason god created the middle finger", "Your face is just fine but we we'll have to put a bag over that personality", "You bring everyone so much joy when you leave the room", "I thought of you today. It reminded me to take out the trash", "@JeppJeppsson Jesper is boring"];
+        let random = Math.floor(Math.random() * roasts.length);
+
+        await msg.reply(roasts[random]);
+    }
+})
+
 client.on("interactionCreate", async interaction => {
 
     if (!interaction.isCommand() || !interaction.guildId) return;
-    
+
     const command = client.commands.get(interaction.commandName);
 
     try {
@@ -65,7 +76,7 @@ client.on("interactionCreate", async interaction => {
 
 async function GetCommands() {
     const clientCommands = fs.readdirSync(`./${process.env.PROD === "true" ? "dist" : "src"}/commands`);
-    
+
     let commandPromises: Array<Promise<void>> = [];
     for (const file of clientCommands) {
         commandPromises.push(new Promise(async (resolve) => {
@@ -77,12 +88,12 @@ async function GetCommands() {
                 console.error(e);
                 resolve();
             }
-            
+
         }));
     }
 
     await Promise.all(commandPromises);
-    client.login(process.env.TOKEN);
+    await client.login(process.env.TOKEN);
 }
 
 async function PlaySong(newState: VoiceState, songPath: string) {
